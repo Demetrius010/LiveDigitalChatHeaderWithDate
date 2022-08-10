932bd2b43e90fa375412f1538380f502110df98d
Dmitry <dmitry.moiseenko@sequenia.com>
2022-08-10 10:09:08 +0700

    Добавлен файл .gitignore

---
+++ example/src/main/java/space/livedigital/chat_sdk/example/ExampleActivity.kt
@@ -0,0 +1,11 @@
+package space.livedigital.chat_sdk.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExampleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.example_activity)
+    }
+}

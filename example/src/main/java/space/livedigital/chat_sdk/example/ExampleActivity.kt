49242c82aab0f4a039274b6906327b3978998299
Dmitry <dmitry.moiseenko@sequenia.com>
2022-08-11 12:26:04 +0700

    example: В Activity добавлена кнопка открытия BotttomSheet чата.

--- example/src/main/java/space/livedigital/chat_sdk/example/ExampleActivity.kt
+++ example/src/main/java/space/livedigital/chat_sdk/example/ExampleActivity.kt
@@ -1,11 +1,35 @@
 package space.livedigital.chat_sdk.example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import space.livedigital.chat_sdk.example.databinding.ExampleActivityBinding
+import space.livedigital.chat_sdk.ui.fragments.dialogs.chat.ChatBottomSheetDialogFragment
 
 class ExampleActivity : AppCompatActivity() {
+
+    private lateinit var binding: ExampleActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.example_activity)
+        binding = ExampleActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupListeners()
+    }
+
+    private fun setupListeners() {
+        binding.chatButton.setOnClickListener { showChat() }
+    }
+
+    private fun showChat() {
+        val chatBottomSheetDialog = ChatBottomSheetDialogFragment()
+        chatBottomSheetDialog.show(supportFragmentManager, TAG)
+    }
+
+    private companion object {
+        const val TAG = "ChatBottomSheet"
     }
 }
